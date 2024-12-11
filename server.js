f9fe75d0ffbb3e848607efdc7005993697de4169
Malek Abasise <abdmalek.abassisse@esprit.tn>
2024-12-12 00:09:44 +0100

    add disk

--- server.js
+++ server.js
@@ -70,7 +70,6 @@ if (process.env.NODE_ENV == "aa") {
 // );
 
 app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
-app.use("/tmp", express.static(path.join(__dirname, "./tmp")));
 
 app.use(express.urlencoded({ extended: true }));
 
